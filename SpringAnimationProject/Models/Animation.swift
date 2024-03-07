1018533d37c68372cb06603301b7cb15fae9b1cd
Muslim Gapparov <muslimgapparov@gmail.com>
2024-03-07 19:54:35 +0300

    Commit

---
+++ SpringAnimationProject/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  SpringAnimationProject
+//
+//  Created by Муслим Гаппаров on 3/4/24.
+//
+
+import Foundation
---
+++ SpringAnimationProject/Services/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  SpringAnimationProject
+//
+//  Created by Муслим Гаппаров on 3/4/24.
+//
+
+import Foundation
